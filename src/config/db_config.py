b784fc574c0a081099e69044e6272aec25401e5f
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-03-02 14:26:53 -0600

    feat(db): src (config)

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -12,7 +12,5 @@ def get_engine():
     db = os.getenv("DB_NAME")
 
     return create_engine(
-        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}",
-        pool_size = 5,
-        max_overflow = 10
+        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
     )
