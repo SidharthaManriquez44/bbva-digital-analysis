ce975315647a3725d5464ff455e2d614294303da
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-03-02 20:00:17 -0600

    updated(db): src (config)

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -1,16 +1,16 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-load_dotenv()
 
 def get_engine():
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-    host = os.getenv("DB_HOST")
-    port = os.getenv("DB_PORT")
-    db = os.getenv("DB_NAME")
+    user = os.environ["DB_USER"]
+    password = os.environ["DB_PASSWORD"]
+    host = os.environ["DB_HOST"]
+    port = os.environ["DB_PORT"]
+    db = os.environ["DB_NAME"]
 
     return create_engine(
-        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
+        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}",
+        pool_size=5,
+        max_overflow=10
     )
